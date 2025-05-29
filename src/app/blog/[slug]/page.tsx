43815a3dc18ff459e2fe58e5a680549c9258429a
Amir <mfathulqorib97@gmail.com>
2025-05-29 14:28:42 +0700

    feat: add new post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -61,7 +61,7 @@ export default async function Blog({ params }: { params: Promise<{ slug: string 
   }
 
   return (
-    <section>
+    <section className="py-8 md:py-12">
       <script
         type="application/ld+json"
         suppressHydrationWarning
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,8 +2,8 @@ import { BlogPosts } from '@/components/posts'
 
 export default function Page() {
   return ( 
-    <section className="py-8 min-h-[calc(100vh-20rem)] md:min-h-[calc(100vh-16rem)]">
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+    <section className="py-8 md:py-12 min-h-[calc(100vh-20rem)] md:min-h-[calc(100vh-16rem)]">
+      <h1 className="font-semibold text-2xl mb-2 md:mb-4 tracking-tighter">My Blog</h1>
       <BlogPosts />
     </section>
   )
